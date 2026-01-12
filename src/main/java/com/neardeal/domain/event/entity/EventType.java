545f4bde7fcd5d739c4c10047d31b99fb652734a
dohyeon <ehgus8910@gmail.com>
2026-01-12 21:31:38 +0900

    feat: 가게 분위기, 이벤트 종류 추가 및 가게 종류에 놀거리 및 뷰티/헬스 추가

---
+++ src/main/java/com/neardeal/domain/event/entity/EventType.java
@@ -0,0 +1,10 @@
+package com.neardeal.domain.event.entity;
+
+public enum EventType {
+
+    FOOD_TRUCK,    // 푸드트럭
+    POPUP_STORE,   // 팝업스토어
+    SCHOOL_EVENT,  // 학교 이벤트
+    FLEA_MARKET,   // 플리마켓
+    PERFORMANCE    //공연/버스킹
+}
--- src/main/java/com/neardeal/domain/store/entity/StoreCategory.java
+++ src/main/java/com/neardeal/domain/store/entity/StoreCategory.java
@@ -4,5 +4,7 @@ public enum StoreCategory {
     BAR,
     CAFE,
     RESTAURANT,
+    ENTERTAINMENT,
+    BEAUTY_HEALTH,
     ETC
 }
